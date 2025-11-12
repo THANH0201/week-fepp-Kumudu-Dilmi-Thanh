11d835cd339feeab476523e8044aa7bc343bd72f
Thanh0201 <thanhho@metropolia.fi>
2025-11-12 15:01:59 +0200

    Thanh- Iteration 3

--- week4-fe-pp/src/components/Services.jsx
+++ week4-fe-pp/src/components/Services.jsx
@@ -5,14 +5,19 @@ import Service from './Service';
 
 function Services() {
   const [servicesData, setServicesData] = useState(services);
-
+  // delete service
+  const removeService = (id) => {
+    const newServices = servicesData.filter((service) => service.id !== id);
+    setServicesData(newServices);
+  };
+  
   return (
     <section className='section services' id='services'>
       <Title title='our' subTitle='services' />
 
       <div className='section-center services-center'>
         {servicesData.map((service) => (
-          <Service {...service} key={service.id} />
+          <Service {...service} key={service.id} removeService={removeService} />
         ))}
       </div>
     </section>
--- week4-fe-pp/src/components/Tours.jsx
+++ week4-fe-pp/src/components/Tours.jsx
@@ -1,18 +1,28 @@
+import { useState } from 'react';
 import { tours } from "../data";
 import Title from "./Title";
 import Tour from "./Tour";
 
-const Tours = () => {
+function Tours() {
+  const [toursData, setToursData] = useState(tours);
+
+  // delete tour
+  const removeTour = (id) => {
+    const newTours = toursData.filter((tour) => tour.id !== id);
+    setToursData(newTours);
+  };
+
   return (
     <section className="section" id="tours">
       <Title title="featured" subTitle="tours" />
 
       <div className="section-center featured-center">
-        {tours.map((tour) => {
-          return <Tour key={tour.id} {...tour} />;
-        })}
+        {toursData.map((tour) => (
+          <Tour key={tour.id} {...tour} removeTour={removeTour} />
+        ))}
       </div>
     </section>
   );
 };
+
 export default Tours;
